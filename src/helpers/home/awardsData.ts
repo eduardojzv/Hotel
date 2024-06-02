6a980973139470dbd108d160372629467c3f3c64
eduardojzv <ezamorava@est.utn.ac.cr>
2024-06-02 15:43:37 -0600

    awards changes

--- src/helpers/home/awardsData.ts
+++ src/helpers/home/awardsData.ts
@@ -4,6 +4,11 @@ interface AwardsData {
     recognitions:string[];
     awardsImgs:string[];
     description02:string;
+    imgKAYAK:string;
+    description03:string;
+    tripAdvisorImgs:string[];
+    description04:string;
+    certificates:string[];
 }
 export const awardsData:AwardsData={
     title:"Premios y Reconocimientos",
@@ -11,5 +16,9 @@ export const awardsData:AwardsData={
     recognitions:["Mejor Resort Familiar de Mexico y Centroamérica","Mejor Todo Incluido de México y Centroamérica","Mejor Resort de Costa Rica"],
     awardsImgs:['awards/award01.png','awards/award02.png','awards/award03.png','awards/award04.png','awards/award05.png','awards/award06.png'],
     description02:"Nuestro Fiesta Resort ha sido reconocido en los KAYAK Travel Awards. El premio se basa en las opiniones de los huéspedes, así que queremos darles nuestro agradecimiento más especial. #KAYAKTravelAwards",
-
+    imgKAYAK:"awards/kayakTravelAwards.png",
+    description03:"Cada año, Tripadvisor reconoce los establecimientos favoritos de los viajeros alrededor del mundo, en función de las reseñas y calificaciones recopiladas durante el año anterior. Gracias a eso estamos entre el 10% de los hoteles más populares del mundo en el selecto grupo de los premios Traveler's Choice Awards 2023.",
+    tripAdvisorImgs:["awards/travelersChoice01.webp","awards/travelersChoice02.webp"],
+    description04:"Contamos con varias certificaciones con reconocimiento nacional e internacional que demuestran nuestro compromiso permanente con los ejes de la sostenibilidad: ambiental, social y económico. Agregamos valor a través de cada actividad que realizamos, minimizamos nuestra huella de carbono con una operación sostenible y compensamos generando impacto positivo en nuestro alrededor.",
+    certificates:["certifications/inteco.png","certifications/blueFlag.png","certifications/essential.png","certifications/acoso.png","certifications/social.png","certifications/red.jpg"]
 }
--- src/sections/home/awards.tsx
+++ src/sections/home/awards.tsx
@@ -3,8 +3,8 @@ import { awardsData } from "../../helpers/home/awardsData";
 
 export default function Awards() {
   return (
-    <div className="h-[750px] grid grid-cols-1 md:grid-cols-2 w-full bg-[url('/public/awards/bgAwards.jpg')] bg-cover bg-center">
-      <div className="flex flex-col text-center items-center gap-4">
+    <div className=" text-white text-lg h-full grid grid-cols-1 md:grid-cols-2 w-full bg-[url('/public/awards/bgAwards.jpg')] bg-cover bg-center p-5">
+      <div className="flex flex-col text-center justify-center items-center gap-2">
         <div>
           <h1>{awardsData.title}</h1>
           <p>{awardsData.description01}</p>
@@ -14,7 +14,7 @@ export default function Awards() {
             <li key={item}>{item}</li>
           ))}
         </ul>
-        <ul className="flex flex-wrap items-center w-auto gap-2">
+        <ul className="flex flex-wrap items-center justify-center gap-2 w-full">
           {awardsData.awardsImgs.map((item) => (
             <li key={item}>
               <Image
@@ -25,8 +25,43 @@ export default function Awards() {
             </li>
           ))}
         </ul>
+        <div className="flex  flex-col items-center justify-center gap-2">
+          <p>{awardsData.description02}</p>
+          <Image
+            shadow="sm"
+            src={awardsData.imgKAYAK}
+            className="w-28 h-28"
+          />
+        </div>
       </div>
-      <div>
+      <div className="flex flex-col text-center justify-center items-center gap-2">
+        <div className="flex flex-col gap-2">
+          <p>{awardsData.description03}</p>
+          <ul className="flex flex-wrap items-center justify-center gap-2 w-full">
+          {awardsData.tripAdvisorImgs.map((item) => (
+            <li key={item}>
+              <Image
+                shadow="sm"
+                src={item}
+                className="w-28 h-28"
+              />
+            </li>
+          ))}
+          </ul>
+          <div>
+           <p> {awardsData.description04}</p>
+           <ul className="flex flex-wrap items-center justify-center gap-2 w-full">
+           {awardsData.certificates.map((item) => (
+            <li key={item}>
+              <Image
+                src={item}
+                className="w-28 h-28"
+              />
+            </li>
+          ))}
+           </ul>
+          </div>
+        </div>
       </div>
     </div>
   )
